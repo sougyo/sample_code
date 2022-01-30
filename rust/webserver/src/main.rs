672df02bcd2b3449dfba6d178b6189fe9621f9f9
Shogo Matsumoto <sougyo2001@gmail.com>
2022-01-30 10:53:15 +0900

    adjust indent

--- rust/webserver/src/main.rs
+++ rust/webserver/src/main.rs
@@ -4,20 +4,20 @@ use std::path::{Path, PathBuf};
 
 #[get("/{filename:.+}")]
 async fn index(req: HttpRequest) -> Result<fs::NamedFile, Error> {
-    let dir_path  = Path::new("public_html");
-    let base_path = PathBuf::from(req.match_info().query("filename"));
-    Ok(fs::NamedFile::open(dir_path.join(base_path))?)
+	let dir_path  = Path::new("public_html");
+	let base_path = PathBuf::from(req.match_info().query("filename"));
+	Ok(fs::NamedFile::open(dir_path.join(base_path))?)
 }
 
 #[get("/")]
 async fn hello() -> impl Responder {
-        HttpResponse::Ok().body("Hello world!")
+	HttpResponse::Ok().body("Hello world!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello).service(index))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+	HttpServer::new(|| App::new().service(hello).service(index))
+		.bind("0.0.0.0:8080")?
+		.run()
+		.await
 }
